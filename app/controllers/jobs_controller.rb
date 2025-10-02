8941f4b050b871ba399b2b5546ebe186177b9c90
Tiago Antunes <tiagoantunes9991@gmail.com>
2025-10-02 11:00:42 +0100

    feat: change the last commit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,12 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: %i[ new create show edit update ]
+  before_action :set_job, only: %i[ create show edit update ]
 
   def index
     @jobs = Job.all.order(created_at: :desc)
   end
 
   def new
+    @job = Job.new
   end
 
   def create
